eb87d7a50a1aa7116db12fe454db847c44d2217c
warprincegas <warprincegas@gmail.com>
2025-04-17 19:08:19 +0100

    deploy config

--- src/homeLayout/Header.jsx
+++ src/homeLayout/Header.jsx
@@ -1,5 +1,7 @@
 import { FaBars, FaMarsAndVenus } from "react-icons/fa6";
 
+// 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
+
 function Header() {
   return (
     <div className="flex items-center justify-between px-3 py-4">
@@ -7,7 +9,7 @@ function Header() {
         <img
           src="logodark.jpg"
           alt="Logo"
-          className="w-[50px] rounded-xl bg-white shadow-lg"
+          className="w-[50px] rounded-xl shadow-lg"
         />
       </div>
       <div className="rounded-md bg-indigo-700 p-1">
